6e6e9fb9589e50ca6c2443c9f8adc8c0bf0cfe2e
ii14 <ii14@users.noreply.github.com>
2021-01-22 05:20:23 +0100

    nvim: updated lsp config

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -1,22 +1,18 @@
 local lspconfig = require 'lspconfig'
-local root_pattern = lspconfig.util.root_pattern
 local util = require 'lsp/util'
 require 'lsp/callbacks'
 
-local configs = require 'lspconfig/configs'
-
 local on_attach = util.on_attach
+-- local root_pattern = lspconfig.util.root_pattern
 
 
--- C/C++
--- TODO: semantic highlighting in clangd. works fine on ccls.
 lspconfig.clangd.setup{
   on_attach = function(client, bufnr)
     on_attach(client, bufnr)
     util.map('n', '<leader>a', ':ClangdSwitchSourceHeader<CR>')
     util.map('n', '<leader>A', ':vs | ClangdSwitchSourceHeader<CR>')
-  end;
-  cmd = {"clangd", "--background-index"};
+  end,
+  cmd = {"clangd", "--background-index"},
 }
 
 -- lspconfig.ccls.setup{
@@ -27,47 +23,59 @@ lspconfig.clangd.setup{
 --   root_dir = root_pattern("compile_commands.json");
 -- }
 
-
--- Python
 lspconfig.pyls.setup{
-  on_attach = on_attach;
+  on_attach = on_attach,
   settings = {
     pyls = {
       plugins = {
-        mccabe = { enabled = false };
-      }
-    }
-  }
+        mccabe = { enabled = false },
+        -- pycodestyle = { enabled = false },
+      },
+    },
+  },
 }
 
--- Golang
 lspconfig.gopls.setup{
-  on_attach = on_attach;
+  on_attach = on_attach,
 }
 
--- Lua
--- lspconfig.sumneko_lua.setup{
---   on_attach = on_attach;
+local sumneko_root_path = '/opt/lua-language-server'
+local sumneko_binary = sumneko_root_path..'/bin/Linux/lua-language-server'
+lspconfig.sumneko_lua.setup{
+  on_attach = on_attach,
+  cmd = {sumneko_binary, '-E', sumneko_root_path .. '/main.lua'},
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+        path = vim.split(package.path, ';'),
+      },
+      diagnostics = {
+        globals = {'vim'},
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand("$VIMRUNTIME/lua/")] = true,
+          [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+        },
+      },
+    },
+  },
+}
+
+-- lspconfig.denols.setup{
+--   on_attach = on_attach,
+--   -- cmd = {'deno', 'lsp', '-L', 'debug'},
+--   init_options = {
+--     config = "./tsconfig.json",
+--   },
 -- }
 
--- Ruby
 -- lspconfig.solargraph.setup{
---   on_attach = on_attach;
+--   on_attach = on_attach,
 --   settings = {
 --     solargraph = {
---       diagnostics = true;
---     }
---   };
--- }
-
--- Dlang
--- configs.dls = {
---   default_config = {
---     cmd = {"dls"};
---     filetypes = {"d"};
---     root_dir = root_pattern("dub.sdl", "dub.json");
---   };
--- }
--- lspconfig.dls.setup{
---   on_attach = on_attach;
+--       diagnostics = true,
+--     },
+--   },
 -- }
