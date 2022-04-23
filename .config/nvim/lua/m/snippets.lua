3c16858b54d6ac7474b7ab02de5081657cc573e1
ii14 <ii14@users.noreply.github.com>
2022-04-23 19:14:58 +0200

    nvim: defer luasnip initialization

--- .config/nvim/lua/m/snippets.lua
+++ .config/nvim/lua/m/snippets.lua
@@ -1,13 +1,30 @@
-local ok, luasnip_config = pcall(require, 'luasnip.config')
-if not ok then return end
+local initialized = false
+local function init()
+  if initialized then return end
+  initialized = true
 
-luasnip_config.set_config {
-  history = true,
-  updateevents = 'TextChanged,TextChangedI',
-  ext_base_prio = 200,
-  ext_prio_increase = 1,
-  enable_autosnippets = true,
-}
+  require('luasnip.config').set_config {
+    history = true,
+    updateevents = 'TextChanged,TextChangedI',
+    ext_base_prio = 200,
+    ext_prio_increase = 1,
+    enable_autosnippets = true,
+  }
+
+  local function imap(lhs, rhs, opts) vim.api.nvim_set_keymap('i', lhs, rhs, opts) end
+  local function smap(lhs, rhs, opts) vim.api.nvim_set_keymap('s', lhs, rhs, opts) end
+  local expr = { silent = true, expr = true }
+  local noremap = { silent = true, noremap = true }
+
+  imap('<Tab>',   [[luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>']], expr)
+  imap('<S-Tab>', [[<cmd>lua require('luasnip').jump(-1)<CR>]], noremap)
+  smap('<Tab>',   [[<cmd>lua require('luasnip').jump(1)<CR>]],  noremap)
+  smap('<S-Tab>', [[<cmd>lua require('luasnip').jump(-1)<CR>]], noremap)
+  -- imap('<C-E>', [[luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<End>']], expr)
+  -- smap('<C-E>', [[luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<End>']], expr)
+  imap('<Esc>', [[<Esc><cmd>silent LuaSnipUnlinkCurrent<CR>]], noremap)
+  smap('<Esc>', [[<Esc><cmd>silent LuaSnipUnlinkCurrent<CR>]], noremap)
+end
 
 local lookup = {
   c          = 'm.snippets.c',
@@ -24,22 +41,9 @@ local lookup = {
 vim.api.nvim_create_autocmd('FileType', {
   desc = '[Snippets] Loads snippets for current filetype',
   callback = function(ctx)
+    init()
     local mod = lookup[ctx.match]
     if mod then require(mod) end
   end,
   group = vim.api.nvim_create_augroup('VimrcSnippets', {}),
 })
-
-local function imap(lhs, rhs, opts) vim.api.nvim_set_keymap('i', lhs, rhs, opts) end
-local function smap(lhs, rhs, opts) vim.api.nvim_set_keymap('s', lhs, rhs, opts) end
-local expr = { silent = true, expr = true }
-local noremap = { silent = true, noremap = true }
-
-imap('<Tab>',   [[luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>']], expr)
-imap('<S-Tab>', [[<cmd>lua require('luasnip').jump(-1)<CR>]], noremap)
-smap('<Tab>',   [[<cmd>lua require('luasnip').jump(1)<CR>]],  noremap)
-smap('<S-Tab>', [[<cmd>lua require('luasnip').jump(-1)<CR>]], noremap)
--- imap('<C-E>', [[luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<End>']], expr)
--- smap('<C-E>', [[luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<End>']], expr)
-imap('<Esc>', [[<Esc><cmd>silent LuaSnipUnlinkCurrent<CR>]], noremap)
-smap('<Esc>', [[<Esc><cmd>silent LuaSnipUnlinkCurrent<CR>]], noremap)
