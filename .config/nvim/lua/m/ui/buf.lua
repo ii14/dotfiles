c4309f9c89263cac668c49dca08d6c5d0a77c771
ii14 <ii14@users.noreply.github.com>
2022-06-21 17:26:09 +0200

    nvim: remove shorten_paths functions from bufferline

--- .config/nvim/lua/m/ui/buf.lua
+++ .config/nvim/lua/m/ui/buf.lua
@@ -9,80 +9,7 @@ local SHORTEN = true
 --- Buffer list
 M.buflist = {}
 
---- Update smart paths
-local function shorten_paths(bufs)
-  do
-    -- TODO: algorithm can enter an infinite loop when you open for example
-    -- `nvim/lua/m/lsp.lua` and `nvim2/lua/m/lsp.lua`. temporary workaround.
-    for _, buf in pairs(bufs) do
-      buf.name = buf.path:match('[^/]*$')
-    end
-    return bufs
-  end
-
-  -- thanks to mengelbrecht/lightline-bufferline for the algorithm
-  local bs = {}
-  local count_per_tail = {}
-
-  for _, buf in ipairs(bufs) do
-    local b = { buf = buf }
-    local name
-    if SHORTEN then
-      name = fn.pathshorten(buf.path)
-    else
-      name = buf.path
-    end
-
-    if name and name ~= '' then
-      b.path = fn.fnamemodify(name, ':p:~:.')
-      local sep = fn.strridx(b.path, '/')
-      if sep ~= -1 and b.path:sub(sep + 1) == '/' then
-        sep = fn.strridx(b.path, '/', sep - 1)
-      end
-      b.sep = sep
-      b.label = b.path:sub(sep + 2)
-      count_per_tail[b.label] = (count_per_tail[b.label] or 0) + 1
-    else
-      b.path = name
-      b.label = name
-    end
-
-    bs[#bs+1] = b
-  end
-
-  while true do
-    local n = 0
-    for k, v in pairs(count_per_tail) do
-      if v > 1 then
-        n = n + 1
-      else
-        count_per_tail[k] = nil
-      end
-    end
-    if n == 0 then
-      break
-    end
-
-    local ambiguous = count_per_tail
-    count_per_tail = {}
-    for _, b in ipairs(bs) do
-      if #b.path > 0 then
-        if b.sep > -1 and ambiguous[b.label] then
-          b.sep = fn.strridx(b.path, '/', b.sep - 1)
-          b.label = b.path:sub(b.sep + 2)
-        end
-        count_per_tail[b.label] = (count_per_tail[b.label] or 0) + 1
-      end
-    end
-  end
-
-  for _, v in ipairs(bs) do
-    local buf = v.buf
-    v.buf = nil
-    buf.name = v.label
-  end
-  return bufs
-end
+-- TODO: shorten buffer names
 
 --- Reload buffer list
 function M.reload()
@@ -98,6 +25,7 @@ function M.reload()
     local buf = {
       bufnr = bufnr,
       path = info.name,
+      name = info.name:match('[^/]*$'),
 
       current = bufnr == curbuf,
       alternate = bufnr == altbuf,
@@ -136,8 +64,6 @@ function M.reload()
   for i = #del, 1, -1 do
     table.remove(M.buflist, del[i])
   end
-
-  shorten_paths(M.buflist)
 end
 
 --- Update buffer properties
